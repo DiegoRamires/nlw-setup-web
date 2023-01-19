7342b7ab6a1e21d7ad8f81a7b67cc306ee569c66
Diego Ramires <goncalves.ramires@gmail.com>
2023-01-19 15:29:11 -0300

    add: showing minimum of year days on table

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,4 +1,6 @@
-import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
+import {
+  generateDatesFromYearBeginning
+} from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -7,13 +9,26 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDateSizes = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDateSizes - summaryDates.length
+
 export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, i) => {
           return (
-            <div key={`${weekDay}-${i}`} className="text-zin-400 text-xl h-10 font-bold flex items-center justify-center">
+            <div
+              key={`${weekDay}-${i}`}
+              className="
+                text-zin-400
+                text-xl
+                h-10
+                font-bold
+                flex
+                items-center
+                justify-center"
+              >
               {weekDay}
             </div>
           )
@@ -24,6 +39,25 @@ export function SummaryTable() {
         {summaryDates.map(date => {
           return (
             <HabitDay key={date.toString()} />
+          )
+        })}
+
+        { amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
+          return (
+            <div
+              key={i}
+              className="
+                w-10
+                h-10
+                bg-zinc-900
+                border-2
+                border-zinc-800
+                rounded-lg
+                opacity-40
+                cursor-not-allowed
+              "
+            >
+            </div>
           )
         })}
       </div>
