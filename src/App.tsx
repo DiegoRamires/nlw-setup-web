bebe0580a49dbce40a95abc23fc32965b2ba40e5
Diego Ramires <goncalves.ramires@gmail.com>
2023-01-19 11:08:26 -0300

    add: create global background on tailwind theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './styles/global.css'
 
-import { Habit } from "./components/Habit"
+// import { Habit } from "./components/Habit"
 
-function App() {
+export function App() {
   return (
     <>
-      <Habit completed={3} />
-      <Habit completed={5} />
-      <Habit completed={13} />
-      <Habit completed={30} />
+
     </>
   )
 }
-
-export default App
