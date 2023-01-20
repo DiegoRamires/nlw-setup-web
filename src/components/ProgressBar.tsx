d8528dda04b8fd66449e502cd00c48759d955af0
Diego Ramires <goncalves.ramires@gmail.com>
2023-01-20 09:29:38 -0300

    add: showing popover with progress when click on a specific day

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBar {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressBar) {
+  return (
+    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+      <div
+        className='h-3 rounded-xl bg-violet-600'
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        style={{ width: `${props.progress}%` }}
+      />
+    </div>
+  )
+}
