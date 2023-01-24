87ef25eddf1faeecfdd3fbd35b1d6843bea9cb3a
Diego Ramires <goncalves.ramires@gmail.com>
2023-01-24 17:25:38 -0300

    chore: disable cursor on past days on list popover

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -63,7 +63,7 @@ export function HabitsList({ date, onCompletedChanged }: HabitListProps) {
             key={habit.id}
             onCheckedChange={() => handleToggleHabit(habit.id)}
             checked={habitsInfo.completedHabits.includes(habit.id)}
-            className='flex items-center gap-3 group focus:outline-none'
+            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
             disabled={isDateInPast}
           >
             <div className='
